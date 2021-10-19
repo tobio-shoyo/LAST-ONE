10aad57691e76c8224e4a46f5533a39f7c559a66
ANKUSH SAHOO <92300611+tobio-shoyo@users.noreply.github.com>
2021-10-19 14:40:33 +0530

    Update imagefiles.py

--- EmiliaAnimeBot/resources/imagefiles.py
+++ EmiliaAnimeBot/resources/imagefiles.py
@@ -2,5 +2,5 @@
 
 #Images in __main__.py
 EMILIA_IMG = "https://telegra.ph/file/b37cec509d121c8c63518.jpg"
-EMILIA_START_IMG = "https://telegra.ph/file/e5fc94ada18369f89c832.jpg"
-EMILIA_HELP_IMG = "https://telegra.ph/file/65fb4fdec1bd9787f01a0.jpg"
+EMILIA_START_IMG = "https://telegra.ph/file/dfec1a3dc126f2b5f779b.jpg"
+EMILIA_HELP_IMG = "https://telegra.ph/file/bf5b29a80aafa051ed894.jpg"
