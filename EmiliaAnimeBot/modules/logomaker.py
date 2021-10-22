6212fea1ba931c4211095ca598cbdd2b4bc8e69b
ANKUSH SAHOO <92300611+tobio-shoyo@users.noreply.github.com>
2021-10-22 23:57:07 +0530

    Update logomaker.py

--- EmiliaAnimeBot/modules/logomaker.py
+++ EmiliaAnimeBot/modules/logomaker.py
@@ -26,14 +26,14 @@ async def lego(event):
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./EmiliaAnimeBot/resources/batmfo__.ttf", 330)
+    font = ImageFont.truetype("./EmiliaAnimeBot/resources/aAbrushow.otf", 330)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
+    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="white")
     fname2 = "LogoByShoyo.png"
     img.save(fname2, "png")
     await tbot.send_file(event.chat_id, fname2, caption="Made By ShoyoRobot")
@@ -60,13 +60,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./EmiliaAnimeBot/resources/back.jpg')
+    img = Image.open('./EmiliaAnimeBot/resources/BG.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./EmiliaAnimeBot/resources/Maghrib.ttf", 1000)
+    font = ImageFont.truetype("./EmiliaAnimeBot/resources/aAlloyInk.ttf", 1000)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
